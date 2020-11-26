a8c5d9a067152503554dd90e241d53c9b8665a5e
randalhsu <randalaaaa@gmail.com>
2020-11-26 16:08:16 +0800

    demosite: Serve app at root URL

--- demosite/urls.py
+++ demosite/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('url/', include('urlshortener.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('urlshortener.urls')),
 ]
